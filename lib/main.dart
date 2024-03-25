8810abfb43542cbb34e7263999e0ad65567a3437
Evgeny Evseev <evgeny@evseevs.ru>
2024-03-25 20:39:05 +0300

    core functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(App());
 }
+
+enum Color { pink, yellow, green }
+
+enum Result { dialog, snackbar }
+
+enum Transition { bottomToTop, topToBottom, rightToLeft }
 
 class App extends StatelessWidget {
   @override
@@ -10,14 +18,73 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Exam Option Selector',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.yellow,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.yellow[50]),
+          ),
+        ),
       ),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  Color? selectedColor;
+  Result? selectedResult;
+  Transition? selectedTransition;
+  Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _randomize();
+  }
+
+  void _randomize() {
+    setState(() {
+      selectedColor = Color.values[random.nextInt(Color.values.length)];
+      selectedResult = Result.values[random.nextInt(Result.values.length)];
+      selectedTransition =
+          Transition.values[random.nextInt(Transition.values.length)];
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Selected options'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Color: $selectedColor'),
+              Text('Result: $selectedResult'),
+              Text('Transition: $selectedTransition'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,19 +99,46 @@ class HomePage extends StatelessWidget {
               'Основной цвет',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            Wrap(
+              alignment: WrapAlignment.spaceAround,
+              spacing: 20,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      setState(() {
+                        selectedColor = Color.pink;
+                      });
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedColor == Color.pink) ? Colors.yellow : null,
+                  ),
                   child: const Text('Розовый'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedColor = Color.yellow;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedColor == Color.yellow) ? Colors.yellow : null,
+                  ),
                   child: const Text('Желтый'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedColor = Color.green;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedColor == Color.green) ? Colors.yellow : null,
+                  ),
                   child: const Text('Зеленый'),
                 ),
               ],
@@ -56,13 +150,32 @@ class HomePage extends StatelessWidget {
             ),
             Wrap(
               alignment: WrapAlignment.spaceAround,
+              spacing: 20,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedResult = Result.dialog;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: (selectedResult == Result.dialog)
+                        ? Colors.yellow
+                        : null,
+                  ),
                   child: const Text('Dialog'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedResult = Result.snackbar;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: (selectedResult == Result.snackbar)
+                        ? Colors.yellow
+                        : null,
+                  ),
                   child: const Text('Snackabr'),
                 ),
               ],
@@ -74,37 +187,71 @@ class HomePage extends StatelessWidget {
             ),
             Wrap(
               alignment: WrapAlignment.spaceAround,
+              spacing: 20,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedTransition = Transition.bottomToTop;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedTransition == Transition.bottomToTop)
+                            ? Colors.yellow
+                            : null,
+                  ),
                   child: const Text('Снизу вверх'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedTransition = Transition.topToBottom;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedTransition == Transition.topToBottom)
+                            ? Colors.yellow
+                            : null,
+                  ),
                   child: const Text('Сверху вниз'),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      selectedTransition = Transition.rightToLeft;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        (selectedTransition == Transition.rightToLeft)
+                            ? Colors.yellow
+                            : null,
+                  ),
                   child: const Text('Справа налево'),
                 ),
               ],
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _randomize,
               child: const Text('Подобрать вариант'),
             ),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Открыть критерии'),
             ),
+            if (selectedColor != null &&
+                selectedResult != null &&
+                selectedTransition != null)
+              ElevatedButton(
+                onPressed: _showDialog,
+                child: const Text('Показать результат'),
+              ),
           ],
         ),
       ),
     );
   }
-
-  // void showDialog(BuildContext context) {}
-  //
-  // void showSnackbar(BuildContext context) {}
 }
